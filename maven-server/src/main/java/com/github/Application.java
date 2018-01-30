fa17d1b9a6c025afa72be46b061b7a4ac55c15d9
yandajun <yandajun@vipkid.com.cn>
2018-01-30 17:02:23 +0800

    create eureka server

---
+++ maven-server/src/main/java/com/github/Application.java
@@ -0,0 +1,23 @@
+package com.github;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description
+ * <p>
+ * </p>
+ * DATE 1/19/18.
+ *
+ * @author yandajun.
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
