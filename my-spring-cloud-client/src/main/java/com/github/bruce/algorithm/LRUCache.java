f6358e60aabeb6b72040644daf32e218400b7e80
yandajun <yandajun@vipkid.com.cn>
2021-01-16 12:26:13 +0800

    LRUCache

---
+++ my-spring-cloud-client/src/main/java/com/github/bruce/algorithm/LRUCache.java
@@ -0,0 +1,37 @@
+package com.github.bruce.algorithm;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    public boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));
+        System.out.println(cache);
+        cache.put(3, 3);
+        System.out.println(cache);
+    }
+}
