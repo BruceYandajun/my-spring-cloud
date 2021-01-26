561fc9797cd204918bbdc72d7d6566b1efa8cd71
yandajun <yandajun@vipkid.com.cn>
2021-01-26 15:10:25 +0800

    ScheduledThreadPool test

--- my-spring-cloud-client/src/main/java/com/github/bruce/concurrent/pools/ScheduledThreadPoolTest.java
+++ my-spring-cloud-client/src/main/java/com/github/bruce/concurrent/pools/ScheduledThreadPoolTest.java
@@ -1,17 +1,25 @@
 package com.github.bruce.concurrent.pools;
 
+import java.util.Date;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadPoolTest {
     private static ScheduledExecutorService executors = Executors.newScheduledThreadPool(2);
 
     public static void main(String[] args) {
         Worker bruce = new Worker("Bruce");
-        executors.schedule(bruce);
+        Worker david = new Worker("david");
+        Worker jack = new Worker("jack");
+        executors.schedule(bruce, 1000L, TimeUnit.MILLISECONDS);
+        executors.schedule(david, 5000L, TimeUnit.MILLISECONDS);
+        executors.schedule(jack, 10000L, TimeUnit.MILLISECONDS);
+        executors.shutdown();
     }
 
-    static class Worker implements Runnable {
+    static class Worker implements Callable<String> {
 
         private String name;
 
@@ -20,8 +28,9 @@ public class ScheduledThreadPoolTest {
         }
 
         @Override
-        public void run() {
-            System.out.println(this.name + " is running!");
+        public String call() {
+            System.out.println(name + " is calling, now is " + new Date());
+            return this.name;
         }
     }
 }
