a4c4593ecde4f73b92e2902f069ce8b0741b2ccd
yandajun <yandajun@vipkid.com.cn>
2021-01-26 16:42:04 +0800

    CompletableFuture translator

--- my-spring-cloud-client/src/main/java/com/github/bruce/concurrent/pools/CompletableFutureTranslator.java
+++ my-spring-cloud-client/src/main/java/com/github/bruce/concurrent/pools/CompletableFutureTranslator.java
@@ -1,16 +1,16 @@
-package com.github.bruce.concurrent;
+package com.github.bruce.concurrent.pools;
 
 import java.util.Random;
 import java.util.concurrent.*;
 
-public class Translator {
+public class CompletableFutureTranslator {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        System.out.println(new Translator().translate("abc"));
+        System.out.println("Completed " + new CompletableFutureTranslator().translate2("abc"));
     }
 
     // todo
-    public String translate(String content) throws InterruptedException, ExecutionException {
+    public String translate1(String content) throws InterruptedException, ExecutionException {
         ExecutorService executor = Executors.newCachedThreadPool();
         ExecutorCompletionService<String> executorCompletionService = new ExecutorCompletionService<>(executor);
         executorCompletionService.submit(() -> baidu(content));
@@ -19,6 +19,17 @@ public class Translator {
         String result = executorCompletionService.take().get();
         executor.shutdown();
         return result;
+    }
+
+    public String translate2(String content) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        CompletableFuture<String> baidu = CompletableFuture.supplyAsync(() -> baidu(content), executor);
+        CompletableFuture<String> google = CompletableFuture.supplyAsync(() -> google(content), executor);
+        CompletableFuture<String> youdao = CompletableFuture.supplyAsync(() -> youdao(content), executor);
+        CompletableFuture<Object> result = CompletableFuture.anyOf(baidu, google, youdao);
+        String result1 = (String) result.get();
+        executor.shutdown();
+        return result1;
     }
 
     public String baidu(String content) {
