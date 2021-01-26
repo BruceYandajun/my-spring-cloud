9390460a714fb398f5b004c5de87252ef54066c4
yandajun <yandajun@vipkid.com.cn>
2021-01-26 20:31:08 +0800

    integer test

---
+++ my-spring-cloud-client/src/main/java/com/github/bruce/base/boxes/IntegerTest.java
@@ -0,0 +1,9 @@
+package com.github.bruce.base.boxes;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        Integer a = 128;
+        Integer b = 128;
+        System.out.println(a == b);
+    }
+}
