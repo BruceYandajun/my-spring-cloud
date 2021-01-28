5deb9acc92112dfa074e2de1ef2dd83db1849c80
yandajun <yandajun@vipkid.com.cn>
2021-01-28 22:39:56 +0800

    bit

--- my-spring-cloud-client/src/main/java/com/github/bruce/algorithm/bitoperation/BitOperation.java
+++ my-spring-cloud-client/src/main/java/com/github/bruce/algorithm/bitoperation/BitOperation.java
@@ -1,4 +1,10 @@
 package com.github.bruce.algorithm.bitoperation;
 
 public class BitOperation {
+
+    public static void main(String[] args) {
+        System.out.println(5 & 6);
+        System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+    }
 }
